98f6c7e7bd77608d88b13c4eea1b1111eea2cde3
Joey O. Chua <jochua@ceu.edu.ph>
2025-01-21 15:12:45 +0800

    add ternary and error suppression

--- variables/operators.php
+++ variables/operators.php
@@ -17,5 +17,19 @@ var_dump($y, $z);
 // $x /= $y; // Equivalent to $x = $x / $y;
 // $x %= $y; // Equivalent to $x = $x % $y;
 // $x **= 2; // Equivalent to $x = $x ** 2;
-
 // echo $x;
+
+// Null Coalescing Operator (if $guess is null, $name will be 'John', 
+//   otherwise $name will be $guess))
+$guess = null;
+$name = $guess ?? 'John'; 
+var_dump($name);
+
+// Ternary Operator (if $switch is true, $light will be 'on', 
+//   otherwise $light will be 'off')
+$switch = true;
+$light = $switch ? 'on' : 'off'; // Value oof $light is now 'on'
+var_dump($light);
+
+echo @$nonexistentVar; // @ will suppress any error messages
+echo $nonexistentVar; // This will display error messages
