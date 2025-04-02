352528bd47401e96f0c42975e79d1e6335aa303c
Joey O. Chua <joeyochua@gmail.com>
2025-04-02 21:08:43 +0800

    init security

---
+++ laravel/security/app/Http/Controllers/BlogController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\BlogRequest;
+use App\Models\Blog;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $blogs = Blog::all();
+        return view('blogs.index', ['blogs' => $blogs]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $users = User::all();
+        $blog = new Blog();
+        return view('blogs.create', ['users' => $users,'blog' => $blog]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(BlogRequest $request)
+    {
+        $blog = new Blog();
+        $blog->title = $request->input('title');
+        $blog->content = $request->input('content');
+        $blog->user_id = $request->input('user_id');
+        $blog->save();
+        return redirect()->route('blogs.show', ['blog' => $blog]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $blog = Blog::findOrFail($id);
+        return view('blogs.show', ['blog' => $blog]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Blog $blog)
+    {
+        $users = User::all();
+        return view('blogs.edit', ['users' => $users,'blog' => $blog]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(BlogRequest $request, string $id)
+    {
+        $blog = Blog::find($id);
+        $blog->title = $request->input('title');
+        $blog->content = $request->input('content');
+        $blog->user_id = $request->input('user_id');
+        $blog->save();
+        return redirect()->route('blogs.show', ['blog' => $blog]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Blog $blog)
+    {
+        $blog->delete();
+        return redirect()->route('blogs.index');
+    }
+}
